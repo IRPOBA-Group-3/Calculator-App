b7922558735f7176fac9566d7591e22e1dcbbc6e
EdisSuljevic <edis.suljevic@stud.hslu.ch>
2026-04-01 18:42:03 +0200

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,29 @@
-public double percent(double a) {
-    return a / 100.0;
-}
+public class Calculator {
 
-public double negate(double a) {
-    return -a;
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
+
+    public double percent(double a) {
+        return a / 100.0;
+    }
+
+    public double negate(double a) {
+        return -a;
+    }
 }
